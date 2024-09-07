479f08d6dc373477626713b0b1db564d095820ec
ru5h16h <solankrushabh7@gmail.com>
2024-09-07 13:51:49 -0400

    Hyperparameter changes in conditional dm

--- configs/cifar10_cc.cfg.py
+++ configs/cifar10_cc.cfg.py
@@ -3,6 +3,7 @@ cfg = {
         "set": "cifar10",
     },
     "train": {
+        "epochs": 250,
         "unet": {
             "sample_size": 32,
             "out_channels": 3,
@@ -10,7 +11,11 @@ cfg = {
                                  "AttnDownBlock2D", "DownBlock2D"),
             "up_block_types":
                 ("UpBlock2D", "AttnUpBlock2D", "UpBlock2D", "UpBlock2D"),
-            "block_out_channels": (64, 128, 256, 256),
+            "block_out_channels": (32, 64, 128, 128),
         },
+        "save_at": [5, 10, 25, 50, 75, 100, 150, 200, 250],
+    },
+    "diffusion": {
+        "infer_at": [1, 5, 10, 25, 50, 75, 100, 150, 200, 250],
     }
 }
