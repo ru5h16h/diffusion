b3b1c05e9d5050ffe3486adf2e05bbab2607ca75
ru5h16h <solankrushabh7@gmail.com>
2024-09-07 23:35:47 -0400

    Configs for inferring fine-tuned mnist

---
+++ configs/infer_mnist_cc_3cl.cfg.py
@@ -0,0 +1,12 @@
+cfg = {
+    "path": {
+        "model":
+            "runs_cc/20240907T091926M647/checkpoints/model_99",
+        "gen_file":
+            "runs_cc/20240907T091926M647/generated_images/{experiment}/plots/{epoch}.png",
+        "ind_path":
+            "runs_cc/20240907T091926M647/generated_images/{experiment}/ind/images/{img_id}.png",
+        "img_lab_path":
+            "runs_cc/20240907T091926M647/generated_images/{experiment}/ind/img_lab.json"
+    },
+}
